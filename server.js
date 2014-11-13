bc241218b9d65c0872ce0a501333d5242dd447b0
Zeeshan Rasool <zrasool88@gmail.com>
2014-11-13 10:20:22 +0000

    cleaning up file structure in prep to merge to development with front-end

--- server.js
+++ server.js
@@ -1,26 +1,34 @@
-var express = require('express')
-var app = express();
-var server = require('http').Server(app);
-var bodyParser = require('body-parser');
-var expressLayouts = require('express-ejs-layouts');
-var connector = require('/src/ghApiConnector');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 var GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 var GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
-
-app.use(bodyParser.urlencoded({ extended: false }))
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.use(expressLayouts);
-app.use('/public', express.static(__dirname + '/public'));
-app.get('/', function(req, res){
-  res.render('index');
-});
-
-
-
-module.exports = server
-if (!module.parent) {
-  console.log('Server running on http://localhost:3000')
-  server.listen(3000)
-}
